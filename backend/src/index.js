d82141e7cb4db1b3a63cede8ec4926e9e3e13442
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2022-03-09 13:39:29 -0300

    apt in frontend with axios

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const routes = require("./routes/api-routes");
 
-app.use("/", routes);
+app.use("/", cors(corsOptions), routes);
 
 app.listen(3001, () => {
   console.log("Server started on port 3001🌐");
--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -6,22 +6,33 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
+  res.json(
+    {msg: "OK!"}
+  )
+})
+
+router.get("/users", (req, res) => {
   res.json({
     data: [
       {
+        id: 1,
         name: "Matheus H",
       },
       {
+        id: 2,
         name: "Sônia M"
       },
       {
+        id: 3,
         name: "Ana C"
       },
       {
+        id: 4,
         name: "Karoline C"
       },
       {
+        id: 5,
         name: "Isabel C"
       }
     ],
