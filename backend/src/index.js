e30571d456edad0d67911b753f65dc60dfbd5cfe
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2022-03-09 18:19:48 -0300

    method post

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 const routes = require("./routes/api-routes");
 
-app.use("/", cors(corsOptions), routes);
+app.use("/", routes);
 
 app.listen(3001, () => {
   console.log("Server started on port 3001🌐");
--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const router = express.Router();
 
 const movies = require("../movies/movies");
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.use((req, res, next) => {
   console.log("Router🌐");
@@ -14,6 +19,15 @@ router.get("/", (req, res) => {
 
 router.get("/movies", (req, res) => {
   res.json(movies);
+});
+
+router.post("/addmovie", (req, res) => {
+  if(req.body.nome && req.body.id && req.body.ano) {
+    movies.push(req.body)
+    res.send("Movie Add")
+  } else {
+    res.send("Movie No Add")
+  }
 });
 
 module.exports = router;
