603a7fefe3d0323e974ecfe7d5942e9a394f6289
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2022-03-09 13:44:10 -0300

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # simple-application-with-api
 Simple application with Api🗃️
+***
+__Simples api com express para estudos📚__
+
+## 📥 Como baixar o projeto
+```bash
+    #Clonar o repositório
+    $ git clone https://github.com/mateushenriquedasilva/simple-application-with-api.git
+
+    #Entrar no diretório
+    $ cd simple-application-with-api
+
+    #Entrar no diretório backend
+    $ cd backend
+
+    #Instalar as dependências
+    $ npm i
+
+    #Iniciar o projeto
+    npm start
+
+    #Sair do diretório
+    $  cd ..
+
+    #Entrar no diretório frontend
+    $ cd frontend
+
+    #Instalar as dependências
+    $ npm i
+
+    #Iniciar o projeto
+    $ npm start
+
+
+```
+
+**About me:**
+- 💻 I'm &lt;Web developer/&gt;
+- ⚛️ JavaScript | TypeScript | NodeJS | React
+- 📩 Talk to me by the discord: **Matheus Henrique#7802**
+- 🧑🏾‍💻 Go to my [Linkedin](https://www.linkedin.com/in/matheus-silva19/)
+- ✍🏿 Write me an <a href="mailto:matheushenriquedasilvaa.2021@gmail.com" target="_blank">Email</a>
+- 👤 Follow me on [Instagram](https://www.instagram.com/themateusreal/)
+- 🖤 Never stop learning
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+interface User {
+  name: string;
+  id: number
 }
 
-export default App
+export default function App() {
+
+  const [users, setUsers] = useState<object[]>([])
+
+  function api() {
+    axios
+      .get("http://localhost:3001/users")
+      .then((res) => {
+        setUsers(res.data.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    api();
+  }, []);
+
+  return <div>
+    {users.map((person: User) => {
+      return <li key={person.id}>{person.name}</li>
+    })}
+  </div>;
+}
